cb24a7804f2064e033a6111742e6316f74291100
mercy-yugi <mercyyugi@gmail.com>
2022-11-05 22:03:47 +0300

    Initial commit

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const classroom = require('./classroom');
+const student = require('./student');
+const lecturer = require('./lecturer');
+const course = require('./course');
+
+
+
+module.exports = {
+  classroom,
+  student,
+  lecturer,
+  course,
+
+};
+
